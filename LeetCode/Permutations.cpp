bf61cbfb675c56a541d356abc23017a61a6aaac9
mufis-coder <muhamadfikrisunandar@gmail.com>
2023-09-12 20:07:23 +0700

    feat: permutations from leetcode

---
+++ LeetCode/Permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void permuteRecursive(vector<int>& nums, int i, int j, vector<vector<int>>& result){
+        if(i==j){
+            result.push_back(nums);
+        }
+        for(int k=i; k<=j; k++){
+            swap(nums[i], nums[k]);
+            permuteRecursive(nums, i+1, nums.size()-1, result);
+            swap(nums[i], nums[k]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        permuteRecursive(nums, 0, nums.size()-1, result);
+        return result;
+    }
+};
