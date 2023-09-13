e7554350b92da654bb0e9ab73fcc4d11207d1fcf
mufis-coder <muhamadfikrisunandar@gmail.com>
2023-09-13 10:22:50 +0700

    feat: swap nodes in pairs leetcode

---
+++ LeetCode/Swap Nodes in Pairs.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addNode(ListNode*& nodes, int val){
+        struct ListNode* newNode = new ListNode(val);
+        if(nodes == NULL) {
+            nodes = newNode;
+        }
+        else{
+            ListNode* curr = nodes;
+            while(curr->next) curr = curr->next;
+            curr->next = newNode;
+        }
+
+        return nodes;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        struct ListNode* result = nullptr;
+        while(head !=NULL){
+            if(head->next != NULL){
+                addNode(result, head->next->val);
+                addNode(result, head->val);
+
+                head = head->next->next;
+            }else{
+                addNode(result, head->val);
+                break;
+            }
+        }
+        return result;
+    }
+};
