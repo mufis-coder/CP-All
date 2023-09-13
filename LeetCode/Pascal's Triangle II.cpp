214d864773a31de99f0b2e2711aecfe29e7f5242
mufis-coder <muhamadfikrisunandar@gmail.com>
2023-09-13 16:40:01 +0700

    feat: pascal triangle II from leetcode

---
+++ LeetCode/Pascal's Triangle II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> result;
+        result.push_back({1});
+        if(rowIndex<1) return result[0];
+        
+        result.push_back({1, 1});
+
+        for(int i=2; i<=rowIndex; i++){
+           vector<int> res;
+           res.push_back(1);
+
+            for(int j=1; j<=i-1; j++){
+                res.push_back(result[i-1][j-1] + result[i-1][j]);
+            }
+
+            res.push_back(1);
+            result.push_back(res);
+        }
+
+        return result[rowIndex];
+    }
+};
