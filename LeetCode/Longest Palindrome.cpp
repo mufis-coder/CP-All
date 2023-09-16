f9eda7aaef1bf37a7cd99b6ffbc7c83463447dfa
mufis-coder <muhamadfikrisunandar@gmail.com>
2023-09-16 07:48:25 +0700

    feat: longest palindrome leetcode

---
+++ LeetCode/Longest Palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> maping;
+
+        for(int i=0; i<s.size(); i++){
+            if(maping.find(s[i]) != maping.end()){
+                maping[char(s[i])] += 1;
+            }else{
+                maping[char(s[i])] = 1;
+            }
+        }
+
+        int odd = 0;
+        int counter = 0;
+        for (auto x : maping) {
+            if(x.second%2 == 0){
+                counter += x.second;
+            }else{
+                if(x.second > 1){
+                    counter += x.second - 1;
+                    odd = 1;
+                }else{
+                    odd = 1;
+                }
+            }
+        }
+        
+        return counter + odd;
+    }
+};
